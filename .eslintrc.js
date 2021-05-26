03c8d8dea4263ff2f8fb8d4dea08445acf0e6e00
Yuliya Platsitsyna <yuliya.platsitsyna@gmail.com>
2021-05-26 14:41:14 +0200

    JSMF2602-77: add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    "indent": ["error", 4, { "SwitchCase": 1 }],
+    "space-before-function-paren": ["error", "never"],
+    "comma-dangle": ["error", "always-multiline"],
+    "space-before-blocks": "error",
+    "object-curly-spacing": ["error", "always"],
+    "no-console": "off",
+    "no-alert": "off",
+    "linebreak-style": 0,
+    "no-var": 0
+  },
+};
