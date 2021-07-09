e1fb9c702d5ad13e46900b3cb4506483f1ce8dd9
Yuliya Platsitsyna <yuliya.platsitsyna@gmail.com>
2021-07-09 19:27:40 +0200

    JSMF2602-163: add HW-24 languages

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,20 @@ import './destinations-turner.js';
 import './destinations-builder.js';
 import './feedback-form.js';
 import './feedback-page.js';
+import './language-select.js';
+import './translations.js';
 import { Clock } from './clock.js';
+import { Translation } from './translations.js';
+import { LanguageMenu } from './language-select.js';
 
 const clock = new Clock({
     clock: document.getElementById('clock')
 });
 
 clock.clockStart();
+
+const t = new Translation();
+t.init();
+t.initPassTranslationEventListener();
+
+const languageMenu = new LanguageMenu(t);
